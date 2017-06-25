080d421eb17c46cb12af942dcff6256f228d3fb4
Jonathan Cua <jonathan.cua@gmail.com>
2017-06-25 14:05:29 -0700

    test1.js

---
+++ test1.js
@@ -0,0 +1,12 @@
+
+<div id="fa"></div>
+
+<script src="https://www.evbqa.com/static/widgets/eb_widgets.js"></script>
+
+<script type="text/javascript">
+        EBWidgets.createWidget({
+        widgetType: 'checkout',
+      eventId: 17899214031,
+      iframeContainerId: 'fa'
+    });
+</script>
